6eb4778ffb358cbaa3e16aa67c2c70eb2ede9e5b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-01-23 23:11:53 +0300

    add

--- 004-service-app/app/sales-api/main.go
+++ 004-service-app/app/sales-api/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	//prefix
+	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	//adding an exported function
+	if err := run(log); err != nil {
+		log.Println("main: error:", err)
+		os.Exit(1)
+
+		//log.Fatalln(v ...interface{})
+	}
+}
+
+func run(log *log.Logger) error {
+	return nil
 }
