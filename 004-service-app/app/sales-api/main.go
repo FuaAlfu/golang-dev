9726a8e4bae0f3aaf8fb2f588078d030c47ce169
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-01-24 21:29:05 +0300

    add

--- 004-service-app/app/sales-api/main.go
+++ 004-service-app/app/sales-api/main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"time"
+
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
 )
+
+// build is the git version of this program. It is set using build flags in the makefile.
+var build = "develop"
 
 func main() {
 	//prefix
@@ -12,11 +19,47 @@ func main() {
 	if err := run(log); err != nil {
 		log.Println("main: error:", err)
 		os.Exit(1)
-
 		//log.Fatalln(v ...interface{})
 	}
 }
 
 func run(log *log.Logger) error {
+		// =========================================================================
+	// Configuration
+
+	var cfg struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:5s"`
+			ShutdownTimeout time.Duration `conf:"default:5s"`
+		}
+	}
+
+	cfg.Version.SVN = build
+	cfg.Version.Desc = "copyright information here"
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module www.github.com/FuaAlfu
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/ardanlabs/conf v1.3.3
+	github.com/gorilla/mux v1.7.4
+	github.com/pkg/errors v0.9.1
+)
