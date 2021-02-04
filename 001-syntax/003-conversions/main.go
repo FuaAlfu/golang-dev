69c01e094af7a66ab52200094ff91403a7f594f9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-02-04 20:26:55 +0300

    conversion

---
+++ 001-syntax/003-conversions/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type foo struct{
+	//memory layout
+	flag bool
+	counter int16
+	pi float32
+}
+
+func main() {
+	fmt.Println("----")
+
+	//declare a variable of an anonymous type and init :: using a struct literal
+	anonymous := struct{
+		flag bool
+		counter int16
+	    pi float32
+	}{
+		flag: true,
+		counter: 100,
+		pi: 3.141592,
+	}
+
+	//create a value of type foo :: zero value
+	var f foo
+
+	//assign the value of the unnamed struct type
+	//to the named struct type value..
+	f = anonymous
+
+	//print the values
+	fmt.Println("%+v\n",f)
+	fmt.Println("%+v\n",anonymous)
+	fmt.Println("flag",anonymous.flag)
+}
