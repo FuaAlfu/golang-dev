d3bd14ae404a1e74c321692d6875931e0afac832
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-02-22 12:48:13 +0300

    add

---
+++ 001-syntax/000-general/004-controlflow--/001-loop/002-whileLoop/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//this is how to code while in Go..
+	x := 1
+	for x < 10 {
+		fmt.Println(x)
+		x++
+	}
+	fmt.Print("done..\n")
+	println("----")
+
+	/*
+		this is how to code while in Go..
+		with if statement..
+	*/
+	xi := 1
+	for {
+		if xi > 9 {
+			break
+		}
+		fmt.Println(xi)
+		xi++
+	}
+	fmt.Print("done..")
+}
