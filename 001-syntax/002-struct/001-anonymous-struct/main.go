aeab9a183ab6d7ad38fea85c26eeb99ab9278ce4
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-03 22:37:40 +0300

    add

---
+++ 001-syntax/002-struct/001-anonymous-struct/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//creating and constructing an anonymous struct
+	randomCar := struct {
+		maker   string
+		model   string
+		color   string
+		mileage int
+	}{
+		maker:   "mercedes-benz",
+		model:   "G63",
+		color:   "red",
+		mileage: 0,
+	}
+
+	fmt.Println("my car's color is", randomCar.color)
+}
