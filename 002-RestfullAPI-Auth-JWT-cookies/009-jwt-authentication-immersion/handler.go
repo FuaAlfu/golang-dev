2fbb96cd2ab87fdca5f0642963edc52d5e660faa
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-04-15 17:38:48 +0300

    add

--- 002-RestfullAPI-Auth-JWT-cookies/009-jwt-authentication-immersion/handler.go
+++ 002-RestfullAPI-Auth-JWT-cookies/009-jwt-authentication-immersion/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
+
 )
 
 //our key for jwt to sign up
@@ -43,13 +44,72 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//todo
+	expirationTime := time.Now().Add(time.Minute * 5)
+	claims := &Claims{
+		Username: credentials.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	tokenString, err := token.SigningString(jwt)
+
+	if err != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(
+		w,
+		&http.Cookie{
+			Name: "token",
+			Value: tokenString,
+			ExpiresAt: expirationTime
+		}
+	)
+
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	cookie, err:= r.Cookie("token")
+	if err != nil{
+		if err == http.ErrNoCookie{
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	tokenString := cookie.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+	func(t *jwt.Token) (interface{}, error){
+		return jwtkey, nil
+	}
+	)
+
+	if err != nil{
+		err == jwt.ErrSingnatureInvalid{
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !tkn.Valid{
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte(ftm.Sprintf("Welcom,%s",claims.Username))) //to user
 }
 
 func Refresh(w http.ResponseWriter, r *http.Request) {
-
+	//todo
 }
