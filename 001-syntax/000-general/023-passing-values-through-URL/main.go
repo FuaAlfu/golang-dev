975f3ba9284c3c3cb091f0abfe590534c1162eb8
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-04 02:37:35 +0300

    add

---
+++ 001-syntax/000-general/023-passing-values-through-URL/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handle()
+	println("servering on port: 8080")
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	fmt.Fprintln(w, "Do my search: "+v)
+}
+
+func handle() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ 001-syntax/000-general/023-passing-values-through-URL/readme.md
@@ -0,0 +1,8 @@
+---
+stack: Go
+pack: net, http
+---
+
+## Passing values through the URL
+We can pass values through the URL.
+We can retrieve them with req.FormValue
