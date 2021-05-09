3874609f12d27c55306c6e0ae9b294e06e2a58c0
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-10 01:48:21 +0300

    add

---
+++ 001-syntax/009-function-idioms/main.go
@@ -0,0 +1,49 @@
+package main
+
+import(
+	"fmt"
+	"encoding/json"
+)
+
+type(
+	user struct{
+		ID int
+		Name string
+		Password string
+	}
+)
+
+func main() {
+	//retrive the user profile
+	u, err := retriveUser("mua")
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	//display the user profile
+	fmt.Println("%+v\n", *u)
+}
+
+//return multaple values out of function , return value and the other state in the stame time
+func retriveUser(name string)(&user, error){
+	//make a call to get the user in json respone..
+	r, err := getUser()
+	if err != nil{
+		return nil, err
+	}
+	/*
+	unmarshal the json document into a value 
+	of the user struct type.
+	*/
+	var u user
+	err = json.Unmarshal([]byte(r), &u); err != nil{
+	return nil, err
+}
+	return &u, nil
+}
+
+//getuser simulates a webcall that returns a json docment for the specified user.
+func getUser(name string)(string,error){
+	response := `{"id": 1234, "name":"mua"}`
+	return response, nil
+}
