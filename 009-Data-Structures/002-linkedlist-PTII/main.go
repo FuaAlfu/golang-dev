ed617a98e23e66b2139875ed07c20a54b8bbdb55
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-12 13:38:10 +0300

    add

---
+++ 009-Data-Structures/002-linkedlist-PTII/main.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+type (
+	node struct {
+		data int //use a single number
+		next *node
+	}
+
+	linkedList struct {
+		head   *node
+		length int //the long of the list
+	}
+)
+
+//defined a pointer reciver method
+func (l *linkedList) prepend(n *node) {
+	second := l.head     //put head in a temp holder
+	l.head = n           //set the new node
+	l.head.next = second //let the new head point to the old head
+	l.length++           //increase the length
+}
+
+//defined a value reciver method
+func (l linkedList) printlistData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next //to update the loop
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+//delete a node with a given value
+func (l *linkedList) deleteWithValue(value int) {
+	//scan over the nodes
+	//compare the data of the next node..
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		previousToDelete = previousToDelete.next //update the loop :: looping till find the match
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length-- //handle the length
+}
+
+func main() {
+	mylist := linkedList{}
+	node1 := &node{data: 23}
+	node2 := &node{data: 11}
+	node3 := &node{data: 8}
+	node4 := &node{data: 97}
+	node5 := &node{data: 42}
+	node6 := &node{data: 31}
+
+	mylist.prepend(node1)
+	mylist.prepend(node2)
+	mylist.prepend(node3)
+	mylist.prepend(node4)
+	mylist.prepend(node5)
+	mylist.prepend(node6)
+
+	fmt.Println(mylist)
+	mylist.printlistData()
+	mylist.deleteWithValue(42)
+	mylist.printlistData()
+}
