96d54d438731a621bc5522334fffabd1c4e25d25
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-15 13:19:57 +0300

    add

--- 009-Data-Structures/004-stacks-and-queues/queue/main.go
+++ 009-Data-Structures/004-stacks-and-queues/queue/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+//Queue
+type Queue struct {
+	items []int
+}
+
+//enqueue :: add a value at the back [end]
+func (q *Queue) enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+//dequeue :: remove a value at the back [end], and returns the removed value
+func (q *Queue) dequeue() int {
+	toRemove := q.items[0]
+	q.items = q.items[1:]
+	return toRemove
+}
+
+func main() {
+	myQueue := Queue{}
+	fmt.Println(myQueue)
+	myQueue.enqueue(5)
+	myQueue.enqueue(15)
+	myQueue.enqueue(25)
+	fmt.Println(myQueue)
+	myQueue.dequeue()
+	fmt.Println(myQueue)
 }
