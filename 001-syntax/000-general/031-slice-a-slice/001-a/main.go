cc6f1c3db77a18cc696115f6e0860e4cbac11150
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-28 20:17:39 +0300

    add

---
+++ 001-syntax/000-general/031-slice-a-slice/001-a/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//a slice
+	s := []int{11, 22, 33, 44}
+
+	//show the second element of slice
+	fmt.Println(s[1])
+
+	//slice [from : Go To]
+	fmt.Println(s[1:])
+
+	//slice from,.. to..
+	fmt.Println(s[1:3])
+
+}
