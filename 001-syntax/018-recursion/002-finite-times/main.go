c6ed763767ae81f214ae62d7161854117dc23a31
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-08-04 16:15:53 +0300

    add

--- 001-syntax/018-recursion/002-finite-times/main.go
+++ 001-syntax/018-recursion/002-finite-times/main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
+
 )
 
 var count = 0
 
-func recursivemethod(limit int) {
+func recursive(limit int) {
 	count++
 	if count <= limit {
 		fmt.Println(count)
-		recursivemethod(limit)
+		recursive(limit)
 	}
 }
 
 func main() {
 	l := 30
-	recursivemethod(l)
+	recursive(l)
 }
