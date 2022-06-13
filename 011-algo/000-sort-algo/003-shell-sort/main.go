17a089f6083903aa8e4bb85686b9ecdb8afd9bc2
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-06-13 21:54:45 +0300

    add

---
+++ 011-algo/000-sort-algo/003-shell-sort/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+/*
+shell sort
+called "diminishing increment sort" too
+*/
+
+func shellSort(data []int) {
+	for i := 1; i < len(data); i++ {
+		if data[i] < data[i-1] {
+			j := i - 1
+			temp := data[i]
+			for j >= 0 && data[j] > temp {
+				data[j+1] = data[j]
+				j--
+			}
+			data[j+1] = temp
+		}
+	}
+	for _, v := range data {
+        fmt.Printf("%v ", v) 
+    }
+}
+
+func main(){
+	s := []int{7, 64, 9, 2, 1, 888, 761, 90}
+	shellSort(s)
+}
